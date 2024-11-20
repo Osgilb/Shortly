91bc682d47f063542d8f01b6eced36f04b56f750
Osgilb <osasgilbert10@gmail.com>
2024-11-20 03:56:52 +0100

    Initial commit

--- js/script.js
+++ js/script.js
@@ -18,3 +18,39 @@ const openMenu = (menu) => {
         Main.classList.remove("mt-24");
     }
 };
+
+const input = document.getElementById('input')
+const form = document.getElementById('form')
+const errMsg = document.getElementById('error')
+
+form.addEventListener('submit', formSubmit)
+
+// Validate a URL
+function validURL(str) {
+    var pattern = new RegExp(
+        '^(https?:\\/\\/)?' + // protocol
+        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
+        '((\\d{1,3}\\.){3}\\d{1,3}))' +
+        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
+        '(\\?[;&a-z\\d%_.~+=-]*)?' +
+        '(\\#[-a-z\\d_]*)?$',
+        'i'
+    )
+    return !!pattern.test(str)
+}
+
+function formSubmit(e) {
+    e.preventDefault()
+
+    if (input.value === '') {
+        errMsg.innerHTML = 'Please enter something'
+        input.classList.add('border-red-400')
+    } else if (!validURL(input.value)) {
+        errMsg.innerHTML = 'Please input a valid URL'
+        input.classList.add('border-red-400')
+    } else {
+        errMsg.innerHTML = ''
+        input.classList.remove('border-red-400')
+        alert('Your link is shorten successfully!')
+    }
+}
